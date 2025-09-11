9c413cd03533f31ad158035c04d2a3a6f724a70d
shivkarsingh <shivkarcse@gmail.com>
2025-09-11 19:30:28 +0530

    api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,34 @@ const express = require("express");
 
 const app = express();
 
+app.get( "/user", (req,res) => {
+
+    const obj = {
+        firstName : "Shivkar",
+        lastName : "Singh"
+    };
+
+    res.send(obj);
+
+});
+
+
+app.post( "/user", (req,res) => {
+
+    // saving to db 
+    res.send("Data successfully saved to db");
+
+});
+
+
+app.delete( "/user", (req,res) => {
+
+    // deleting it
+    res.send("Deleted Successfully");
+    
+});
+
+
 app.listen( 3000, () => {
     console.log( "Server is successfully listening on port 3000..." );
 });
